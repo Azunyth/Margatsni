37783d795fc16b8c54137900bd8312ff479ed415
Azunyth <alexis.vernot@gmail.com>
2017-02-11 16:30:48 +0100

    Add feature to add comment

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,22 @@
 $(document).ready(function() {
 
-    var test = "Hello #bonjour asf #test";
-
     $('span.comment').each(function(idx, elem) {
         var currentHtml = $(this).html();
         $(this).html(lookingForHashtag(currentHtml));
+    });
+
+    $('form').on('submit', function(e) {
+        e.preventDefault();
+
+        var newComment = lookingForHashtag($(this).children('input').val());
+
+        var structureComment = '<li><div><p><span class="author"><a href="">azunyth</a></span> <span class="comment">'+
+            newComment + '</span></p></div></li>';
+
+        $(this).parents('.post-informations-comment').siblings('ul').append(structureComment);
+
+        $(this).children('input').val('');
+
     });
 
 });
