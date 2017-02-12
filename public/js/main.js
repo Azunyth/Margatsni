5559e4ce724c490d14e3cbb81dfbf112f206ad2c
Azunyth <alexis.vernot@gmail.com>
2017-02-12 13:24:43 +0100

    Add like animation on double click on picture

--- public/js/main.js
+++ public/js/main.js
@@ -19,7 +19,41 @@ $(document).ready(function() {
 
     });
 
+    $('div.post-picture').on('dblclick', function(e){
+        var likePost = $(this).siblings('div.post-informations').find('a.like-post');
+
+        if(likePost.children('span').hasClass('glyphicon-heart-empty')) {
+            var heart = $('<span class="like-animation glyphicon glyphicon-heart"></span>');
+            heart.appendTo($(this));
+
+            heart.animate({opacity: 0.8}, 200, function() {
+                heart.delay(300).animate({opacity: 0.0}, 300, function() {
+                    heart.remove();
+                });
+                likePost.trigger('liking');
+            });
+
+        }
+    });
+
+    $('a.like-post').on('click liking', function(e) {
+        var span = $(this).children('span');
+        var classToCheck = 'glyphicon-heart-empty';
+        if(span.hasClass(classToCheck)) {
+            span.removeClass(classToCheck);
+            span.addClass('glyphicon-heart');
+
+            incLikeCounter(span.parents('.post-informations').find('.count'));
+        }
+    });
+
 });
+
+var incLikeCounter = function(elem) {
+    var currentLikeCount = parseInt(elem.html(), 10);
+
+    elem.html(++currentLikeCount);
+}
 
 var lookingForHashtag = function(sentence) {
     var regexp = /#[a-zA-Z]+/g;
